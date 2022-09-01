260bb0549540d2a6b062575c5520a29e0ed0c1da
root_MLH <haddon.mike2@outlook.com>
2022-08-31 19:10:21 -0500

    sub-url requests

--- index.jsx
+++ index.jsx
@@ -6,6 +6,15 @@ const port = 5000;
 const users = require('./routes/users.jsx');
 const items = require('./routes/items.jsx');
 
+// Simple request timer
+app.use('/items', (req, res, next) => {
+  console.log('A request was made at ' + Date.now());
+  next();
+});
+
+app.use('/users', users);
+app.use('/items', items);
+
 app.get('/', (req, res) => {
   res.send("Hello World");
 });
@@ -14,13 +23,17 @@ app.get('/:id', (req, res) => {
   res.send('The request that you made is ' + req.params.id);
 });
 
+app.get('/things/:id([0-9]{5})', function(req, res){
+  res.send('id: ' + req.params.id);
+});
+
+app.get('*', function(req, res){
+  res.send('Sorry, this is an invalid URL.');
+});
+
 app.post('/', (req, res) => {
   res.send(`You just called the post method at '/'!\n`);
 });
-
-app.use('/users', users);
-app.use('/items', items);
-
 
 app.listen(port, () => {
   console.log(`Express listening on port ${port}`);
--- routes/items.jsx
+++ routes/items.jsx
@@ -5,6 +5,10 @@ router.get('/', (req, res) => {
   res.send('GET route to items.');
 });
 
+router.get('/:id', (req, res) => {
+  res.send('Item ID is... ' + req.params.id);
+});
+
 router.post('/', (req, res) => {
   res.send('POST route to items.');
 });
